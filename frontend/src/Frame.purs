8ea5d0188ff9c9bf9e11817e4ae18e1c8d432688
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-04-12 23:47:31 +0200

    refact: exports everything

--- frontend/src/Frame.purs
+++ frontend/src/Frame.purs
@@ -17,13 +17,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-module Frame
-       (
-         mainFrame,
-         Query,
-         Message
-       )
-       where
+module Frame where
 
 import Data.Maybe (Maybe(..))
 import Halogen as H
